7a90439b2d0766d4b6737d7e94a42d1127dada77
Brendan Gibat <brendan.gibat@listhub.com>
2015-06-29 16:05:09 -0400

    setting value

--- logstash.go
+++ logstash.go
@@ -65,7 +65,7 @@ func (a *LogstashAdapter) Stream(logstream chan *router.Message) {
 	if err == nil {
 		value, err := ioutil.ReadAll(resp.Body)
 		if err == nil {
-			instance_id = instance_id
+			instance_id = value
 		}
 	}
 	resp.Body.Close()
