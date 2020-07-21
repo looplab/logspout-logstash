f6992c03834b75f8888a7fda4cdf88863747999e
Connor <clake1@godaddy.com>
2020-07-21 03:20:59 -0700

    Fixed bugs related to INCLUDE_CONTAINERS (#65)
    * Fixed bugs related to INCLUDE_CONTAINERS
    
    * variable rename
    
    * Added regex support
    
    * Docs about regex option INCLUDE_CONTAINERS_REGEX

--- logstash.go
+++ logstash.go
@@ -7,10 +7,11 @@ import (
 	"log"
 	"net"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 
-	"github.com/fsouza/go-dockerclient"
+	docker "github.com/fsouza/go-dockerclient"
 	"github.com/gliderlabs/logspout/router"
 )
 
@@ -20,11 +21,11 @@ func init() {
 
 // LogstashAdapter is an adapter that streams UDP JSON to Logstash.
 type LogstashAdapter struct {
-	conn             net.Conn
-	route            *router.Route
-	containerTags    map[string][]string
-	logstashFields   map[string]map[string]string
-	decodeJsonLogs   map[string]bool
+	conn           net.Conn
+	route          *router.Route
+	containerTags  map[string][]string
+	logstashFields map[string]map[string]string
+	decodeJsonLogs map[string]bool
 }
 
 // NewLogstashAdapter creates a LogstashAdapter with UDP as the default transport.
@@ -142,7 +143,6 @@ func GetContainerHostname(c *docker.Container) string {
 // Stream implements the router.LogAdapter interface.
 func (a *LogstashAdapter) Stream(logstream chan *router.Message) {
 
-	logLoop:
 	for m := range logstream {
 
 		dockerInfo := DockerInfo{
@@ -152,14 +152,9 @@ func (a *LogstashAdapter) Stream(logstream chan *router.Message) {
 			Hostname: GetContainerHostname(m.Container),
 		}
 
-		// If INCLUDE_CONTAINERS is set, check if this container is included
-		if includeContainers := os.Getenv("INCLUDE_CONTAINERS"); includeContainers != "" {
-			for _, containerName := strings.Split(includeContainers, ",") {
-				if dockerInfo.Name == containerName {
-					// skip this
-					continue logLoop
-				}
-			}
+		// Check if we are sending logs for this container
+		if !containerIncluded(dockerInfo.Name) {
+			continue
 		}
 
 		if os.Getenv("DOCKER_LABELS") != "" {
@@ -218,6 +213,31 @@ func (a *LogstashAdapter) Stream(logstream chan *router.Message) {
 			}
 		}
 	}
+}
+
+// containerIncluded Returns true if this container is in INCLUDE_CONTAINERS, or not env var is set
+func containerIncluded(inputContainerName string) bool {
+	if includeContainers := os.Getenv("INCLUDE_CONTAINERS"); includeContainers != "" {
+		for _, containerName := range strings.Split(includeContainers, ",") {
+			if inputContainerName == containerName {
+				// This contain is included, send this log
+				return true
+			}
+		}
+		return false
+	} else if includeContainers := os.Getenv("INCLUDE_CONTAINERS_REGEX"); includeContainers != "" {
+		compiled, err := regexp.Compile(includeContainers)
+		if err != nil {
+			// Return true by default
+			return true
+		}
+		if compiled.MatchString(inputContainerName) {
+			// This contain is included, send this log
+			return true
+		}
+		return false
+	}
+	return true
 }
 
 type DockerInfo struct {
